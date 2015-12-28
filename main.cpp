655dd77291df8447a68b0626c02ad55520d6e0a0
Makemek <makemek101@hotmail.com>
2015-12-28 21:39:35 +0700

    use function predefinition

--- main.cpp
+++ main.cpp
@@ -11,88 +11,8 @@
 #include "threadInfo.h"
 #include "ntinfo.h"
 
-std::vector<DWORD> threadList(DWORD pid) {
-	// solution from http://stackoverflow.com/questions/1206878/enumerating-threads-in-windows
-	std::vector<DWORD> vect = std::vector<DWORD>();
-	HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-	if (h == INVALID_HANDLE_VALUE)
-		return vect;
-
-	THREADENTRY32 te;
-	te.dwSize = sizeof(te);
-	if (Thread32First(h, &te)) {
-		do {
-			if (te.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) +
-				sizeof(te.th32OwnerProcessID)) {
-
-				
-				if (te.th32OwnerProcessID == pid) {
-					printf("PID: %04d Thread ID: 0x%04x\n", te.th32OwnerProcessID, te.th32ThreadID);
-					vect.push_back(te.th32ThreadID);
-				}
-					
-			}
-			te.dwSize = sizeof(te);
-		} while (Thread32Next(h, &te));
-	}
-
-	return vect;
-}
-
-DWORD GetThreadStartAddress(HANDLE processHandle, HANDLE hThread) {
-	// rewritten from https://github.com/cheat-engine/cheat-engine/blob/master/Cheat%20Engine/CEFuncProc.pas#L3080
-	DWORD used = 0, ret = 0;
-	DWORD stacktop = 0, result = 0;
-
-	MODULEINFO mi;
-		
-	GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
-	stacktop = (DWORD)GetThreadStackTopAddress_x86(processHandle, hThread);
-
-	// The stub below has the same result as calling GetThreadStackTopAddress()
-	// LPCVOID tebBaseAddress = GetThreadTebBaseAddress(processHandle, hThread);
-	//if (tebBaseAddress)
-	//	ReadProcessMemory(processHandle, (LPCVOID)((DWORD)tebBaseAddress + 4), &stacktop, 4, NULL);
-
-	// 32 bit - not work -- can't get GetThreadContext()
-	//CONTEXT context;
-	//LDT_ENTRY ldtentry;
-	//GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
-	//
-	//if (GetThreadContext(processHandle, &context)) {
-	//	
-	//	if (GetThreadSelectorEntry(hThread, context.SegFs, &ldtentry)) {
-	//		ReadProcessMemory(processHandle,
-	//			(LPCVOID)( (DWORD*)(ldtentry.BaseLow + ldtentry.HighWord.Bytes.BaseMid << ldtentry.HighWord.Bytes.BaseHi << 24) + 4),
-	//			&stacktop,
-	//			4,
-	//			NULL);
-	//	}
-	//}
-
-	CloseHandle(hThread);
-
-	if (stacktop) {
-		//find the stack entry pointing to the function that calls "ExitXXXXXThread"
-		//Fun thing to note: It's the first entry that points to a address in kernel32
-
-		DWORD* buf32 = new DWORD[4096/4];
-		
-		if (ReadProcessMemory(processHandle, (LPCVOID)(stacktop - 4096), buf32, 4096, NULL)) {
-			for (int i = 4096 / 4 - 1; i >= 0; --i) {
-				if (buf32[i] >= (DWORD)mi.lpBaseOfDll && buf32[i] <= (DWORD)mi.lpBaseOfDll + mi.SizeOfImage) {
-					result = stacktop - 4096 + i * 4;
-					break;
-				}
-					
-			}
-		}
-
-		delete buf32;
-	}
-
-	return result;
-}
+std::vector<DWORD> threadList(DWORD pid);
+DWORD GetThreadStartAddress(HANDLE processHandle, HANDLE hThread);
 
 bool isGameAvail;
 
@@ -136,4 +56,87 @@ int main(int argc, char** argv) {
 	}
 
 	return EXIT_SUCCESS;
+}
+
+std::vector<DWORD> threadList(DWORD pid) {
+	// solution from http://stackoverflow.com/questions/1206878/enumerating-threads-in-windows
+	std::vector<DWORD> vect = std::vector<DWORD>();
+	HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (h == INVALID_HANDLE_VALUE)
+		return vect;
+
+	THREADENTRY32 te;
+	te.dwSize = sizeof(te);
+	if (Thread32First(h, &te)) {
+		do {
+			if (te.dwSize >= FIELD_OFFSET(THREADENTRY32, th32OwnerProcessID) +
+				sizeof(te.th32OwnerProcessID)) {
+
+
+				if (te.th32OwnerProcessID == pid) {
+					printf("PID: %04d Thread ID: 0x%04x\n", te.th32OwnerProcessID, te.th32ThreadID);
+					vect.push_back(te.th32ThreadID);
+				}
+
+			}
+			te.dwSize = sizeof(te);
+		} while (Thread32Next(h, &te));
+	}
+
+	return vect;
+}
+
+DWORD GetThreadStartAddress(HANDLE processHandle, HANDLE hThread) {
+	// rewritten from https://github.com/cheat-engine/cheat-engine/blob/master/Cheat%20Engine/CEFuncProc.pas#L3080
+	DWORD used = 0, ret = 0;
+	DWORD stacktop = 0, result = 0;
+
+	MODULEINFO mi;
+
+	GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
+	stacktop = (DWORD)GetThreadStackTopAddress_x86(processHandle, hThread);
+
+	// The stub below has the same result as calling GetThreadStackTopAddress()
+	// LPCVOID tebBaseAddress = GetThreadTebBaseAddress(processHandle, hThread);
+	//if (tebBaseAddress)
+	//	ReadProcessMemory(processHandle, (LPCVOID)((DWORD)tebBaseAddress + 4), &stacktop, 4, NULL);
+
+	// 32 bit - not work -- can't get GetThreadContext()
+	//CONTEXT context;
+	//LDT_ENTRY ldtentry;
+	//GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
+	//
+	//if (GetThreadContext(processHandle, &context)) {
+	//	
+	//	if (GetThreadSelectorEntry(hThread, context.SegFs, &ldtentry)) {
+	//		ReadProcessMemory(processHandle,
+	//			(LPCVOID)( (DWORD*)(ldtentry.BaseLow + ldtentry.HighWord.Bytes.BaseMid << ldtentry.HighWord.Bytes.BaseHi << 24) + 4),
+	//			&stacktop,
+	//			4,
+	//			NULL);
+	//	}
+	//}
+
+	CloseHandle(hThread);
+
+	if (stacktop) {
+		//find the stack entry pointing to the function that calls "ExitXXXXXThread"
+		//Fun thing to note: It's the first entry that points to a address in kernel32
+
+		DWORD* buf32 = new DWORD[4096 / 4];
+
+		if (ReadProcessMemory(processHandle, (LPCVOID)(stacktop - 4096), buf32, 4096, NULL)) {
+			for (int i = 4096 / 4 - 1; i >= 0; --i) {
+				if (buf32[i] >= (DWORD)mi.lpBaseOfDll && buf32[i] <= (DWORD)mi.lpBaseOfDll + mi.SizeOfImage) {
+					result = stacktop - 4096 + i * 4;
+					break;
+				}
+
+			}
+		}
+
+		delete buf32;
+	}
+
+	return result;
 }
