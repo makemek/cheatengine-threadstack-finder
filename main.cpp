dbf99251a330d7f3456e306fc066e6efde997898
Makemek <makemek101@hotmail.com>
2015-12-28 21:13:33 +0700

    cleanup unused code

--- main.cpp
+++ main.cpp
@@ -1,68 +1,15 @@
-#include <windows.h>
 #include <iostream>
 #include <string>
-#include <ctime>
-#include <cstdint>
 #include <vector>
+
+#include <windows.h>
+
+// list all PIDs and TIDs
 #include <tlhelp32.h>
 #include <Psapi.h>
 
 #include "threadInfo.h"
 #include "ntinfo.h"
-
-// TODO: FIND the way to obtain threadstack0 address
-// change this everytim when I run cheat engine browse it to mem region -> go to address -> threadstack0
-#define THREADSTACK0 0xF9FDF0
-#define PLAYBACK_BASE_ADDRESS  THREADSTACK0 - 0x318
-#define UPDATE_MILLS 100
-
-//typedef LONG    NTSTATUS;
-//typedef NTSTATUS(WINAPI *pNtQIT)(HANDLE, LONG, PVOID, ULONG, PULONG);
-//#define STATUS_SUCCESS    ((NTSTATUS)0x00000000L)
-//#define ThreadQuerySetWin32StartAddress 9
-//
-//DWORD WINAPI _GetThreadStartAddress(HANDLE hThread)
-//{
-//	NTSTATUS ntStatus;
-//	HANDLE hDupHandle;
-//	DWORD dwStartAddress;
-//
-//	pNtQIT NtQueryInformationThread = (pNtQIT)GetProcAddress(GetModuleHandle("ntdll.dll"), "NtQueryInformationThread");
-//	
-//	if (NtQueryInformationThread == NULL)
-//		return 0;
-//
-//	HANDLE hCurrentProcess = GetCurrentProcess();
-//	if (!DuplicateHandle(hCurrentProcess, hThread, hCurrentProcess, &hDupHandle, THREAD_QUERY_INFORMATION, FALSE, 0)) {
-//		SetLastError(ERROR_ACCESS_DENIED);
-//
-//		return 0;
-//	}
-//
-//	
-//	ntStatus = NtQueryInformationThread(hThread, ThreadQuerySetWin32StartAddress, &dwStartAddress, sizeof(DWORD), NULL);
-//	CloseHandle(hDupHandle);
-//	if (ntStatus != STATUS_SUCCESS)
-//		return 0;
-//
-//	return dwStartAddress;
-//
-//}
-
-DWORD getVirtualAddress(int pointerLevel, HANDLE hProcHandle, DWORD offset[], DWORD baseAddress) {
-	DWORD pointer = baseAddress;
-	DWORD pTemp;
-
-	DWORD pointerAddr;
-
-	ReadProcessMemory(hProcHandle, (LPCVOID)pointer, &pTemp, sizeof(pTemp), NULL);
-	for (int c = 0; c < pointerLevel; ++c) {
-		pointerAddr = pTemp + offset[c];
-		ReadProcessMemory(hProcHandle, (LPCVOID)pointerAddr, &pTemp, sizeof(pTemp), NULL);
-	}
-	
-	return pointerAddr;
-}
 
 std::vector<DWORD> threadList(DWORD pid) {
 	std::vector<DWORD> vect = std::vector<DWORD>();
@@ -80,7 +27,7 @@ std::vector<DWORD> threadList(DWORD pid) {
 				
 				if (te.th32OwnerProcessID == pid) {
 					printf("PID: %04d Thread ID: 0x%04x\n", te.th32OwnerProcessID, te.th32ThreadID);
-				vect.push_back(te.th32ThreadID);
+					vect.push_back(te.th32ThreadID);
 				}
 					
 			}
@@ -89,28 +36,6 @@ std::vector<DWORD> threadList(DWORD pid) {
 	}
 
 	return vect;
-}
-
-DWORD GetModuleBase(LPSTR lpModuleName, DWORD dwProcessId)
-{
-	MODULEENTRY32 lpModuleEntry = { 0 };
-	HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwProcessId);
-
-	if (!hSnapShot)
-		return NULL;
-	lpModuleEntry.dwSize = sizeof(lpModuleEntry);
-	BOOL bModule = Module32First(hSnapShot, &lpModuleEntry);
-	while (bModule)
-	{
-		if (!strcmp(lpModuleEntry.szModule, lpModuleName))
-		{
-			CloseHandle(hSnapShot);
-			return (DWORD)lpModuleEntry.modBaseAddr;
-		}
-		bModule = Module32Next(hSnapShot, &lpModuleEntry);
-	}
-	CloseHandle(hSnapShot);
-	return NULL;
 }
 
 DWORD GetThreadStartAddress(HANDLE processHandle, HANDLE hThread) {
@@ -123,31 +48,26 @@ DWORD GetThreadStartAddress(HANDLE processHandle, HANDLE hThread) {
 	GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
 	stacktop = (DWORD)GetThreadStackTopAddress_x86(processHandle, hThread);
 
-	// same as GetThreadStackTopAddress
+	// The stub below has the same result as calling GetThreadStackTopAddress()
 	// LPCVOID tebBaseAddress = GetThreadTebBaseAddress(processHandle, hThread);
 	//if (tebBaseAddress)
 	//	ReadProcessMemory(processHandle, (LPCVOID)((DWORD)tebBaseAddress + 4), &stacktop, 4, NULL);
 
-	
-
-	// 32 bit - not work at GetThreadContext()
-	/*HANDLE hDupHandle;
-	DuplicateHandle(processHandle, hThread, processHandle, &hDupHandle, THREAD_QUERY_INFORMATION, TRUE, 0);
-	
-	GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
-
-	std::cout << GetThreadContext(processHandle, &context) << std::endl;
-
-	if (GetThreadContext(hDupHandle, &context)) {
-		
-		if (GetThreadSelectorEntry(hThread, context.SegFs, &ldtentry)) {
-			ReadProcessMemory(processHandle,
-				(LPCVOID)( (DWORD*)(ldtentry.BaseLow + ldtentry.HighWord.Bytes.BaseMid << ldtentry.HighWord.Bytes.BaseHi << 24) + 4),
-				&stacktop,
-				4,
-				NULL);
-		}
-	}*/
+	// 32 bit - not work -- can't get GetThreadContext()
+	//CONTEXT context;
+	//LDT_ENTRY ldtentry;
+	//GetModuleInformation(processHandle, LoadLibrary("kernel32.dll"), &mi, sizeof(mi));
+	//
+	//if (GetThreadContext(processHandle, &context)) {
+	//	
+	//	if (GetThreadSelectorEntry(hThread, context.SegFs, &ldtentry)) {
+	//		ReadProcessMemory(processHandle,
+	//			(LPCVOID)( (DWORD*)(ldtentry.BaseLow + ldtentry.HighWord.Bytes.BaseMid << ldtentry.HighWord.Bytes.BaseHi << 24) + 4),
+	//			&stacktop,
+	//			4,
+	//			NULL);
+	//	}
+	//}
 
 	CloseHandle(hThread);
 
@@ -173,20 +93,19 @@ DWORD GetThreadStartAddress(HANDLE processHandle, HANDLE hThread) {
 	return result;
 }
 
-bool updateOnNextRun;
 bool isGameAvail;
 
 int main(int argc, char** argv) {
 	std::string gameName = "osu!";
 	LPCSTR LGameName = "osu!";
-	DWORD playbackOffsets[] = { 0x194, 0x228, 0x154, 0x134, 0x2EC };
-
+	
 	HWND hGameWindow = NULL;
 	DWORD dwProcID = NULL;
 	HANDLE hProcHandle = NULL;
 	isGameAvail = false;
 	std::string gameStatus;
 
+	// polling until target process is opened
 	std::cout << "Looking for " << gameName << std::endl;
 	while (!isGameAvail) {
 		hGameWindow = FindWindow(NULL, LGameName);
@@ -208,8 +127,6 @@ int main(int argc, char** argv) {
 		}
 	}
 	
-	//std::cout << std::hex << GetModuleBase("osu!.exe", dwProcID) << std::endl;
-
 	std::vector<DWORD> threadId = threadList(dwProcID);
 	int stackNum = 0;
 	for (auto it = threadId.begin(); it != threadId.end(); ++it) {
